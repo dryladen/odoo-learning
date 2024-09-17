fc260b2511944fea20af9b1c59d0f5a9140d30f4
Laden <delfanladen24@gmail.com>
2024-09-17 17:08:44 +0800

    adding menu in todolist

--- custome_addons/todolist/models/models.py
+++ custome_addons/todolist/models/models.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class todolist(models.Model):
-#     _name = 'todolist.todolist'
-#     _description = 'todolist.todolist'
+class Todolist(models.Model):
+    _name = 'todolist.todolist'
+    _description = 'Table for todo data'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    is_done = fields.Boolean(string='Is Done')
+    deadline = fields.Date(string='Deadline')
+    user_id = fields.Many2one('res.users', string='User')
+    color = fields.Integer(string='Color Index')
+    priority = fields.Selection([
+        ('0', 'Low'),
+        ('1', 'Normal'),
+        ('2', 'High')
+    ], string='Priority', default='1')
+    # name = fields.Char()
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    # description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
